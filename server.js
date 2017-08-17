c866bdcb0496b21a67b68972387f16d938aa4213
Gildon Kirk <gildonkirk@gmail.com>
2017-08-17 15:08:12 -0400

    default route works

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ var path = require('path');
 var PORT = 3000;
 
 app.get('/survey', function(req, res) {
-	res.sendFile(path.join(__dirname, 'app/public/survey.html'))
-})
+	res.sendFile(path.join(__dirname, 'app/public/survey.html'));
+});
 
-
+app.use(function(req, res) {
+	res.sendFile(path.join(__dirname, 'app/public/home.html'));
+});
 
 
 
