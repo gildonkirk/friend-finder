a50cc505129bfcfa9e107eb445373991a09a930a
Gildon Kirk <gildonkirk@gmail.com>
2017-08-17 14:54:59 -0400

    connection to server working, path to survey page working

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 var express = require('express');
+var app = express();
 var parser = require('body-parser');
 var path = require('path');
+
+var PORT = 3000;
+
+app.get('/survey', function(req, res) {
+	res.sendFile(path.join(__dirname, 'app/public/survey.html'))
+})
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+	console.log('Listening on PORT: ' + PORT);
+})
