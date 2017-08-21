ad167c9ad7efbd1a37fd8af055831206b4cdc0a4
Gildon Kirk <gildonkirk@gmail.com>
2017-08-21 19:39:18 -0400

    reduce fucntion added.  get difference score between two friends, need to add functionality for multiple comparisons

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var friends = [
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name":"Gil",
+    "photo":"https://scontent.fphl1-2.fna.fbcdn.net/v/t31.0-8/10847456_10203337655234342_6188989751099400333_o.jpg?oh=609e2e437f61d27f140d2637dc35e273&oe=5A24DDBB",
     "scores":[
         5,
-        1,
-        4,
+        3,
         4,
         5,
-        1,
-        2,
+        5,
         5,
         4,
-        1
+        4,
+        4,
+        5
     ]
   }
 ];
